e6a0c66e4117870c1b2831cec3e234ae8b4a4b9d
hw <0526shw@gmail.com>
2023-01-27 18:16:19 +0900

    js최신문법 완료

---
+++ 1.js최신문법/11.export,import(1).js
@@ -0,0 +1,9 @@
+const hello = {
+    world: 'Hello World!',
+    bye: 'Goodbye world..',
+    zoom: 'I hate zoom eye bad'
+}
+
+const moon = "full moon"
+
+export { hello, moon }
---
+++ 1.js최신문법/12.export,import(2).js
@@ -0,0 +1,3 @@
+import {hello, moon} from './11.export,import(1)'
+console.log(hello.world)
+console.log(moon)
